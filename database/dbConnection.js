aedb5cc3cac4e4a15ce329e02e93a7dfa068aaa7
Sahitya Tiwari <sahityatiwari90035@gmail.com>
2024-06-23 01:53:47 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
 
 const dbConnection = () => {
-    mongoose.connect("mongodb+srv://sahitya7:Sahitya@2001@foodheavencluster.gwdbzq8.mongodb.net/foodheavendatabase?retryWrites=true&w=majority&appName=foodheavencluster", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false
-    })
-    .then(() => {
-        console.log("Database Connected");
-    })
-    .catch(err => {
-        console.log("Database Connection Error:", err);
-    });
-}
+    const MONGO_URI = "mongodb+srv://sahitya7:Sahitya@2001@foodheavencluster.gwdbzq8.mongodb.net/foodheavendatabase?retryWrites=true&w=majority";
 
+    mongoose.connect(MONGO_URI)
+        .then(() => {
+            console.log("Database Connected");
+        })
+        .catch(err => {
+            console.error("Database Connection Error:", err);
+        });
+};
 
 module.exports = { dbConnection };
+
