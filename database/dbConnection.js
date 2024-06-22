831f97aa1702462ea2d33c151b7679583e881fbe
Sahitya Tiwari <sahityatiwari90035@gmail.com>
2024-06-23 01:46:30 +0530

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -4,8 +4,8 @@ const dbConnection = () => {
     mongoose.connect("mongodb+srv://sahitya7:Sahitya@2001@foodheavencluster.gwdbzq8.mongodb.net/foodheavendatabase?retryWrites=true&w=majority&appName=foodheavencluster", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+        // useCreateIndex: true,
+        // useFindAndModify: false
     })
     .then(() => {
         console.log("Database Connected");
